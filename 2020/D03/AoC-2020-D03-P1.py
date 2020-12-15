7d925650e4072539bc9a051c89f817ae7272c700
Jack-Baird <58047143+Jack-Baird@users.noreply.github.com>
2020-12-15 19:43:12 +0000

    Completed Day 3!

--- 2020/D03/AoC-2020-D03-P1.py
+++ 2020/D03/AoC-2020-D03-P1.py
@@ -1,2 +1,25 @@
 # AoC 2020, Day 03, Part 1
 # https://adventofcode.com/2020/day/3
+
+#[x]: Read in input from text file
+#[x]: Iterate over lines, moving RIGHT 3 each time you move down 1
+#[x]: Repeat input frame as required if idx > len(frame)
+#[x]: Determine how many trees (#) would be encountered on the way down the slope
+
+with open('2020/D03/D03-Input.txt', 'r') as infile:
+    frame = infile.read().splitlines()
+
+trees = 0
+
+for i in range(len(frame)):
+
+    row = frame[i]
+    idx = (i * 3) % len(row)
+
+    # print(len(row), idx, 'ERROR' if idx > len(row) else 'OK')
+
+    if row[idx] == '#':
+
+        trees += 1
+
+print(f'Riding on this slope, you would encouter {trees} trees!')
--- 2020/D03/AoC-2020-D03-P2.py
+++ 2020/D03/AoC-2020-D03-P2.py
@@ -1,2 +1,40 @@
 # AoC 2020, Day 03, Part 2
 # https://adventofcode.com/2020/day/3
+
+#[x]: Do the same as Part 1, but also for 3 other slopes
+#[x]: For the fifth and final slope, work out how to change the RIGHT:DOWN ratio to 1:2
+
+from math import prod
+
+with open('2020/D03/D03-Input.txt', 'r') as infile:
+    frame = infile.read().splitlines()
+
+# List of slope gradients (RIGHT, DOWN)
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+]
+
+tree_counts = []
+
+for slope in slopes:
+
+    trees = 0
+
+    for i in range(0, len(frame), slope[1]):
+
+        row = frame[i]
+        idx = (i * slope[0]) % len(row)
+
+        # print(len(row), idx, 'ERROR' if idx > len(row) else 'OK')
+
+        if row[idx] == '#':
+
+            trees += 1
+
+    tree_counts.append(trees)
+
+print(f'Combined, all slopes have {prod(tree_counts)} trees along their given paths!')
